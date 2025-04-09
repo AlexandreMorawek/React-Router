38bea60907a93cd3f509d4b548390fe87e0e27a7
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-04-09 10:30:39 +0200

    Pages statiques

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, Link, RouterProvider } from 'react-router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css'
+
+
+import Home from "./pages/Home.tsx"
+import About from './pages/About.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <Home />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <About />
+        </main>
+      </>
+    ),
+  },
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
