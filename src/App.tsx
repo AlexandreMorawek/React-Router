710c9b2e3c68baa1fee3ce15e52741fe27a8481a
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-04-09 11:25:12 +0200

    adding several articles

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,12 @@ function App() {
   return (
     <>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <Link to="/"><button>Home</button></Link>
+        <Link to="/about"><button>About</button></Link>
+        <Link to="/articles/1"><button>Article 1</button></Link>
+        <Link to="/articles/2"><button>Article 2</button></Link>
+        <Link to="/articles/3"><button>Article 3</button></Link>
+        <Link to="/articles/4"><button>Article 4</button></Link>
       </nav>
       <main>
         <Outlet />
--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,32 @@
 import ReactDOM from "react-dom/client"
-import { createBrowserRouter, Link, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
 import './index.css'
+import App from "./App.tsx";
 
-
-import Home from "./pages/Home.tsx"
-import About from './pages/About.tsx'
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Article from "./pages/Article.tsx";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: (
-      <>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-        <main>
-          <Home />
-        </main>
-      </>
-    ),
-  },
-  {
-    path: "/about",
-    element: (
-      <>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-        <main>
-          <About />
-        </main>
-      </>
-    ),
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/articles/:id",
+        element: <Article />,
+      },
+    ],
   },
 ]);
-
 // rendering
 
 const rootElement = document.getElementById("root");
